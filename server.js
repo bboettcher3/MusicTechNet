96fe08708fba952c34ed0509e2e9120888c13064
Brady Boettcher <bboettcher2@wisc.edu>
2022-04-30 16:02:49 -0400

    server file fix

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/network', function(req, res) {
   const filename = __dirname + "/data/MTNetwork_" + req.query.idx + ".json";
-  var file = new File(filename);
-  if (file.exists()) {
+  if (fs.existsSync(filename)) {
     let netJson = JSON.parse(fs.readFileSync(filename));
     res.json(netJson); // Set disposition and send it.
   }
